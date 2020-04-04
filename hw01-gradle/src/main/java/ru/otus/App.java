9bda6e155bfcd99c225c01e2b94fc3cedf2e0c30
alittleprince <alittleprince@yandex-team.ru>
2020-04-04 12:33:43 +0500

    add hw app

---
+++ hw01-gradle/src/main/java/ru/otus/App.java
@@ -0,0 +1,13 @@
+package ru.otus;
+
+import com.google.common.collect.Maps;
+import java.util.HashMap;
+
+
+public class App {
+    public static void main(String... args) {
+        HashMap<String, String> example = Maps.newHashMap();
+        example.put("hello", "otus");
+        System.out.println(example);
+    }
+}
