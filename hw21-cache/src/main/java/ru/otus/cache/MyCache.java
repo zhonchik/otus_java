f15849ffd753440bc36e8035ae3535c3b4ce6aff
alittleprince <alittleprince@yandex-team.ru>
2020-07-18 15:42:24 +0500

    fixed constants naming

--- hw21-cache/src/main/java/ru/otus/cache/MyCache.java
+++ hw21-cache/src/main/java/ru/otus/cache/MyCache.java
@@ -1,35 +1,38 @@
 package ru.otus.cache;
 
+import lombok.extern.java.Log;
+
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
 
+@Log
 public class MyCache<K, V> implements HwCache<K, V> {
 
     private final Map<K, V> cache = new WeakHashMap<>();
     private final Set<HwListener<K, V>> listeners = new HashSet<>();
 
-    private final static String getAction = "get";
-    private final static String putAction = "put";
-    private final static String removeAction = "remove";
+    private static final String GET_ACTION = "get";
+    private static final String PUT_ACTION = "put";
+    private static final String REMOVE_ACTION = "remove";
 
     @Override
     public void put(K key, V value) {
         cache.put(key, value);
-        notifyListeners(key, value, putAction);
+        notifyListeners(key, value, PUT_ACTION);
     }
 
     @Override
     public void remove(K key) {
         var value = cache.remove(key);
-        notifyListeners(key, value, removeAction);
+        notifyListeners(key, value, REMOVE_ACTION);
     }
 
     @Override
     public V get(K key) {
         var value = cache.get(key);
-        notifyListeners(key, value, getAction);
+        notifyListeners(key, value, GET_ACTION);
         return value;
     }
 
